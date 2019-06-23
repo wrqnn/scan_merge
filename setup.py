d3b0ad52a1047a3e822d5bf834df845e86c33c7d
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2019-06-23 21:17:06 +0200

    Fix error.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
-        'Topic :: TODO',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
