0f32556e27a59eaa869f85a41a5a982db3130c58
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2022-07-30 14:04:28 +0200

    Reformat with black.

--- src/scan_merge.py
+++ src/scan_merge.py
@@ -23,37 +23,37 @@ def interleave_longest(*sequences):
 def main():
     """Main entry point"""
     parser = argparse.ArgumentParser(
-        description=
-        'Tool to merge single-sided PDF scans of the same page stack into a double sided scan'
+        description="Tool to merge single-sided PDF scans of the same page stack into a double sided scan"
     )
-    pdf_completer = argcomplete.FilesCompleter(allowednames=('pdf',))
-    parser.add_argument("front_file",
-                        type=Path,
-                        help="Path to PDF with fronts of pages (1,3,...)").completer = pdf_completer
-    parser.add_argument("back_file",
-                        type=Path,
-                        help="Path to PDF with backs of pages (2,4,...)").completer = pdf_completer
-    parser.add_argument("output_file",
-                        type=Path,
-                        help="Path of combined output file").completer = pdf_completer
+    pdf_completer = argcomplete.FilesCompleter(allowednames=("pdf",))
+    parser.add_argument(
+        "front_file", type=Path, help="Path to PDF with fronts of pages (1,3,...)"
+    ).completer = pdf_completer
+    parser.add_argument(
+        "back_file", type=Path, help="Path to PDF with backs of pages (2,4,...)"
+    ).completer = pdf_completer
+    parser.add_argument(
+        "output_file", type=Path, help="Path of combined output file"
+    ).completer = pdf_completer
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
     output = PdfFileWriter()
     # Load the PDFs
-    with args.front_file.open(mode='rb') as front_file, \
-            args.back_file.open(mode='rb') as back_file:
+    with args.front_file.open(mode="rb") as front_file, args.back_file.open(
+        mode="rb"
+    ) as back_file:
         input_front = PdfFileReader(front_file)
         input_back = PdfFileReader(back_file)
         # Generate combined sequence of pages from both PDFs
         combined = filter(
-            bool,
-            interleave_longest(input_front.pages, reversed(input_back.pages)))
+            bool, interleave_longest(input_front.pages, reversed(input_back.pages))
+        )
         for page in combined:
             output.addPage(page)
-        with args.output_file.open(mode='wb') as output_file:
+        with args.output_file.open(mode="wb") as output_file:
             output.write(output_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
